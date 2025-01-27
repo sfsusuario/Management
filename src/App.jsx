1af8ae38b0b5c1e6c5eac5d6a6a4a912c40598b1
samael.fierro <samael.fierro@n5now.com>
2025-01-27 10:30:11 -0500

    FX: Add titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Management from "./components/Management";
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <title>Task Management Board</title>
+        <meta name="description" content="A drag and drop task management board for organizing projects and tracking progress" />
+      </Helmet>
       <Management />
     </div>
   );
